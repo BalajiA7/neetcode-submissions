8e63dfdf9312afae0db0c2b4bdd084938b62e659
Balaji <52853391+BalajiA7@users.noreply.github.com>
2026-08-05 17:31:10 +0530

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.py
@@ -0,0 +1,32 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        seen = set()
+        group = defaultdict(list)
+
+        for i in range(len(strs)):
+            key = tuple(sorted(strs[i]))
+            group[key].append(strs[i])
+        
+        return list(group.values())
+            
+            # if strs[i] not in seen:
+            #     res = []
+            #     res.append(strs[i])
+            #     seen.add(strs[i])
+            #     mapA = {}
+            #     for s in strs[i]:
+            #         mapA[s] = mapA.get(s, 0) + 1
+
+            #     for j in range(i+1, len(strs)):
+            #         # check anagram
+            #         mapB = {}
+            #         for s in strs[j]:
+            #             mapB[s] = mapB.get(s, 0) + 1
+                    
+            #         if mapA == mapB:
+            #             seen.add(strs[j])
+            #             res.append(strs[j])
+
+            #     group.append(res)
+
+        
