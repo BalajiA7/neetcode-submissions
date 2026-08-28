1e60b3d2b0f76ed90928441470b4808cea157c62
Balaji <52853391+BalajiA7@users.noreply.github.com>
2026-08-28 10:48:17 +0530

    Add: k-closest-points-to-origin - submission-1

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        # for each coordinate calc distance between origin
+        distances = []
+        for val in points:
+            x, y = val
+            distance  = x*x + y*y
+            distances.append([distance,val])
+        
+        # now put that array into heap either i can sort the array elements
+        distances.sort()
+        print(distances)
+        # finally return the k closes point in the list
+        result = []
+        for i in range(k):
+            result.append(distances[i][1])
+        
+        return result
+
