cbf5f77e00d493d598f9406c046fe16c33313554
Balaji <52853391+BalajiA7@users.noreply.github.com>
2026-09-24 11:31:14 +0530

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.py
@@ -0,0 +1,32 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adjacencyList = defaultdict(list)
+        visited = [0] * numCourses
+       
+        for a,b in prerequisites:
+            adjacencyList[b].append(a)
+        
+        def dfs(i):
+            if visited[i] == 1:
+                return False
+            
+            if visited[i] == 2:
+                return True
+
+            visited[i] = 1
+            for neighbours in adjacencyList[i]:
+                if not dfs(neighbours):
+                    return False
+
+            visited[i] = 2
+            return True
+
+        for i in range(numCourses):
+            if not dfs(i):
+                return False
+
+        return True
+        
+
+
+        
