a1edd5b9be63c60c7ec0692e46fef65063dee229
Balaji <52853391+BalajiA7@users.noreply.github.com>
2026-09-25 22:04:12 +0530

    Add: course-schedule-ii - submission-1

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-1.py
@@ -0,0 +1,34 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adjacencyList = defaultdict(list)
+        visited = [0] * numCourses
+        res = []
+       
+        for a,b in prerequisites:
+            adjacencyList[a].append(b)
+        
+        def dfs(i):
+            if visited[i] == 1:
+                return False
+            
+            if visited[i] == 2:
+                return True
+
+            visited[i] = 1
+            for neighbours in adjacencyList[i]:
+                if not dfs(neighbours):
+                    return False
+
+            visited[i] = 2
+            res.append(i)
+            return True
+        
+        for i in range(numCourses):
+            if not dfs(i):
+                return []
+
+        return res
+        
+
+
+        
